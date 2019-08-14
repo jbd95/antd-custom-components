e245ea4ee1c234cc5fa1149fcabca2254e4b89bb
jbd95 <james.brady2@mavs.uta.edu>
2019-08-14 02:14:23 -0500

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# Ant Design Custom Components
--- src/components/Link.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Link = (props) => (
-    <a href={props.href} target="_blank" rel='noopener noreferrer' className='black-color' onClick={props.onClick}>
-        {props.content}  
-    </a>
-);
